8f1b2fe46be16f94551a42edbc8888f036b44e20
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2020-01-13 18:39:31 -0500

    Add more boilerplate functionality.

--- boilerplate/boil.py
+++ boilerplate/boil.py
@@ -1,42 +1,22 @@
 #!/usr/bin/env python3
 
 import json
-import re
 import sys
 
-from jinja2 import Environment, FileSystemLoader
-
+import inflect as inflect
 import yaml
+from jinja2 import Environment, FileSystemLoader
 from jsonschema import validate, ValidationError
 
 
 def read_yaml(yaml_file_name):
     with open(yaml_file_name, "r") as f:
-        return yaml.load(f)
+        return yaml.safe_load(f)
 
 
 def read_json(json_file_name):
     with open(json_file_name, "r") as f:
         return json.load(f)
-
-
-def kebab_case(s):
-    return s.lower().replace('-', '_')
-
-
-def snake_case(s):
-    return s.lower().replace('-', '_')
-
-
-def camel_case(s):
-    words = re.split('[^A-Za-z0-9]', s)
-    first = words[0].lower()
-    rest = [word.capitalize() for word in words[1:]]
-    return "".join([first] + rest)
-
-
-def tab(string, n=1):
-    print(" " * n * 4, string)
 
 
 def rule(n, char='='):
@@ -52,14 +32,14 @@ def banner(string, fat=False):
         print("\n" + string + "\n")
 
 
-def comment(s):
-    print(f"\n# ---- {s}\n")
-
-
 schema = read_json("schemata/nest-schema.json")
 
 jinja2_env = Environment(loader=FileSystemLoader("./templates"))
-template = jinja2_env.get_template("entity.ts.jinja2")
+entity_template = jinja2_env.get_template("entity.ts.jinja2")
+resolver_template = jinja2_env.get_template("resolver.ts.jinja2")
+service_template = jinja2_env.get_template("service.ts.jinja2")
+
+p = inflect.engine()
 
 for file_name in sys.argv[1:]:
     if file_name.endswith(".json"):
@@ -67,10 +47,20 @@ for file_name in sys.argv[1:]:
     else:
         variables = read_yaml(file_name)
 
+    if 'namePlural' not in variables:
+        variables['namePlural'] = p.plural(variables['name'])
+
     try:
         validate(variables, schema)
     except ValidationError as err:
         print("Validation failed", err)
         exit(1)
 
-    print(template.render(variables))
+    banner(f"Entity for {file_name}", True)
+    print(entity_template.render(variables))
+
+    banner(f"Service for {file_name}", True)
+    print(service_template.render(variables))
+
+    banner(f"Resolver for {file_name}", True)
+    print(resolver_template.render(variables))
